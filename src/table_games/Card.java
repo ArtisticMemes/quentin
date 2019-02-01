098127be6119dc840886b6b57dad10e0838ab5bc
ArtisticMemes <bryanchristopherjohnson@gmail.com>
2019-02-01 13:39:37 -0500

    Add Card class & code formatter

---
+++ src/table_games/Card.java
@@ -0,0 +1,18 @@
+package table_games;
+
+public class Card
+{
+
+    public Card()
+    {
+        // TODO Auto-generated constructor stub
+
+    } // Card()
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    } // main()
+
+} // Card
