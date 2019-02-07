ac06b36f47f202cf4b494227c13df8fa4a047470
Maannss <46467013+Maannss@users.noreply.github.com>
2019-02-07 13:21:25 -0500

    added doc to DeckTester

--- src/table_games/test/DeckTester.java
+++ src/table_games/test/DeckTester.java
@@ -1,17 +1,22 @@
 package table_games.test;
 
 import table_games.*;
-
+/**
+ * This is a class that tests the Deck class.
+ */
 public class DeckTester
 {
     public static void main(String[] args)
     {
+      //variables 
       String[] ranks = {"yike","oof","darn"};
       String[] sute = {"hart","dimon","clud","shovel"};
       int[] values = {1,2,3,4,5,6,7,8,9,10};
       
+      //creating the deck
       Deck deck = new Deck(ranks,sute,values);
       
+      //the testing part
       System.out.println("the deck  \n"+deck);
       System.out.println("size of the deck " + deck.size());
       deck.deal();
