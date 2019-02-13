c76792dac55ad6b5276257a57ac47469ad58aac3
ArtisticMemes <bryanchristopherjohnson@gmail.com>
2019-02-13 17:24:41 -0500

    Fix perfectShuffle()
    Made the temporary value equal to the start of the array because that is the value that is lost when going from the start to the end. Added in testing for shuffles

--- src/table_games/Shuffler.java
+++ src/table_games/Shuffler.java
@@ -6,6 +6,61 @@ public class Shuffler
 {
     public static final int DEFAULT_RANDOM_MIN = 0;
 
+    /**
+     * The number of consecutive shuffle steps to be performed in each call to each
+     * sorting procedure.
+     */
+    private static final int SHUFFLE_COUNT = 20;
+
+    /**
+     * The number of values to shuffle.
+     */
+    private static final int VALUE_COUNT = 6;
+
+    /**
+     * Tests shuffling methods.
+     * 
+     * @param args is not used.
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Results of " + SHUFFLE_COUNT + " consecutive perfect shuffles:");
+        int[] values1 = new int[VALUE_COUNT];
+        for(int i = 0; i < values1.length; i++)
+        {
+            values1[i] = i;
+        }
+        for(int j = 1; j <= SHUFFLE_COUNT; j++)
+        {
+            perfectShuffle(values1);
+            System.out.print("  " + j + ":");
+            for(int k = 0; k < values1.length; k++)
+            {
+                System.out.print(" " + values1[k]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        System.out.println("Results of " + SHUFFLE_COUNT + " consecutive efficient selection shuffles:");
+        int[] values2 = new int[VALUE_COUNT];
+        for(int i = 0; i < values2.length; i++)
+        {
+            values2[i] = i;
+        }
+        for(int j = 1; j <= SHUFFLE_COUNT; j++)
+        {
+            selectionShuffle(values2);
+            System.out.print("  " + j + ":");
+            for(int k = 0; k < values2.length; k++)
+            {
+                System.out.print(" " + values2[k]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    } // main()
+
     public static boolean arePermutations(int[] a, int[] b)
     {
         if(a.length != b.length)
@@ -23,12 +78,12 @@ public class Shuffler
         if(array.length == 0)
             return;
 
-        int end = array[array.length - 1];
+        int start = array[0];
         for(int i = 0; i < array.length - 1; i++)
         {
             array[i] = array[i + 1];
         }
-        array[0] = end;
+        array[array.length - 1] = start;
     } // perfectShuffle()
 
     // Randomize int array
